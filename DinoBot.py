8a9c6b37fb1bb49cf64befcd2959cc8545ec96ff
Priyanshu Mittal <priyanshumittal8@gmail.com>
2022-07-13 02:56:59 +0530

    update code

--- DinoBot.py
+++ DinoBot.py
@@ -1,13 +1,16 @@
-import time
-import pyautogui
-import cv2
-import mss
 import numpy
+import mss
+import cv2
+import webbrowser
+import pyautogui
+import time
 
+url = "https://chromedino.com/"
+webbrowser.open(url)
 
 with mss.mss() as sct:
     # Part of the screen to capture
-    monitor = {"top": 370, "left": 315, "width": 50, "height": 30}
+    monitor = {"top": 395, "left": 220, "width": 10, "height": 10}
     img1 = numpy.array(sct.grab(monitor))
     while "Screen capturing":
 
@@ -15,11 +18,11 @@ with mss.mss() as sct:
 
         # Get raw pixels from the screen, save it to a Numpy array
         img2 = numpy.array(sct.grab(monitor))
-        if(numpy.any(cv2.absdiff(img1, img2))):  # If there is a difference in image1 and image2 then
-            pyautogui.press('space')    # Press space key to jump above cactus
+        if (numpy.any(cv2.absdiff(img1, img2))):  # If there is a difference in image1 and image2 then
+            pyautogui.press('up')    # Press space key to jump above cactus
 
         # Display the picture
-        #cv2.imshow("OpenCV/Numpy normal", img1)
+        # cv2.imshow("OpenCV/Numpy normal", img1)
 
         # Display FPS of game
         print("fps: {}".format(1 / (time.time() - last_time)))
